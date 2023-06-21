45ad4280767fcb44ff1fddebbc900a53b252dfea
bermudezanajulia <anajuliabermudez321@gmail.com>
2023-06-21 16:02:28 -0300

    aprendendo sobre mvvm

--- src/modules/personagem.js
+++ src/modules/personagem.js
@@ -5,4 +5,18 @@ export class Personagem {
     level
     tipo
     descricao
+
+    constructor(nome, level, tipo){
+        this.nome = nome
+        this.level = level
+        this.tipo = tipo
+    }
+
+
+    obterInsignia(){
+        if(this.level >= 5){
+            return `Implacavel ${this.tipo}`
+        }
+        return `${this.tipo}  Iniciante`
+    }
 }
