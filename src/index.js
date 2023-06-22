98f7bcc90c65a2d3f3c1d84e819d667d85d5ec53
bermudezanajulia <anajuliabermudez321@gmail.com>
2023-06-22 11:10:13 -0300

    adicionei as cartas

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import {Personagem} from "./modules/personagem.js"
+import { PersonagemView } from "./components/personagem-view.js"
+import { Mago } from "./modules/mago.js"
 
 const personagemPedrinho = new Personagem('Pedrinho',5,'Mago')
 const personagemJose = new Personagem('Jose', 3, 'Arqueiro')
+
+const personagens = [personagemPedrinho, personagemJose]
+
+new PersonagemView(personagens).render()
+
+console.log(new Mago('gelo', 7, 9))
